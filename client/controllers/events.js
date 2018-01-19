dc24f14e38b2e03f7dc040f8521d917a27a6cd19
Amit Ghosh <amitbd1508@gmail.com>
2018-01-20 03:43:44 +0600

    remove unnessery calender code and add some comment in event controler

--- client/controllers/events.js
+++ client/controllers/events.js
@@ -3,6 +3,7 @@ var myApp = angular.module('myApp');
 myApp.controller('EventsController', ['$scope', '$http', '$location', '$routeParams','moment', 'calendarConfig', function($scope, $http, $location, $routeParams,moment,  calendarConfig){
     console.log('EventsController loaded...');
 
+    //mobile view detector
     var isNotMobile = (function() {
         var check = false;
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
@@ -35,69 +36,29 @@ myApp.controller('EventsController', ['$scope', '$http', '$location', '$routePar
         console.log(event);
         $scope.calendarView=viewType
     };
+
+    //on calendar add edit and delete button
     var actions = [{
-        label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+        label: '<i class=\'glyphicon glyphicon-plus\'></i>',
         onClick: function(args) {
-            console.log(args.calendarEvent.id);
-            window.location.href='#!/event/update/'+args.calendarEvent.id;
+            window.location.href='#!/event/add/';
 
         }
     }, {
-        label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+        label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
         onClick: function(args) {
-            //alert.show('Deleted', args.calendarEvent);
-            console.log(args.calendarEvent);
+            window.location.href='#!/event/update/'+args.calendarEvent.id;
+        }
+
+    }, {
+        label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+        onClick: function (args) {
+            $scope.removeEvent(args.calendarEvent.id);
         }
     }];
 
     $scope.cellIsOpen = true;
 
-
-    $scope.eventClicked = function(event) {
-        //alert.show('Clicked', event);
-        console.log(event);
-    };
-
-    $scope.eventEdited = function(event) {
-        //alert.show('Edited', event);
-        console.log(event);
-    };
-
-    $scope.eventDeleted = function(event) {
-        //alert.show('Deleted', event);
-        console.log("Delete");
-        console.log(event);
-    };
-
-    $scope.eventTimesChanged = function(event) {
-        //alert.show('Dropped or resized', event);
-        console.log(event);
-    };
-
-    $scope.toggle = function($event, field, event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        event[field] = !event[field];
-    };
-
-    $scope.timespanClicked = function(date, cell) {
-
-        if ($scope.calendarView === 'month') {
-            if (($scope.cellIsOpen && moment(date).startOf('day').isSame(moment($scope.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
-                $scope.cellIsOpen = false;
-            } else {
-                $scope.cellIsOpen = true;
-                $scope.viewDate = date;
-            }
-        } else if ($scope.calendarView === 'year') {
-            if (($scope.cellIsOpen && moment(date).startOf('month').isSame(moment($scope.viewDate).startOf('month'))) || cell.events.length === 0) {
-                $scope.cellIsOpen = false;
-            } else {
-                $scope.cellIsOpen = true;
-                $scope.viewDate = date;
-            }
-        }
-    }
 
     //api call for get all events
     $scope.getEvents = function(){
@@ -132,7 +93,7 @@ myApp.controller('EventsController', ['$scope', '$http', '$location', '$routePar
 
 
 
-    //api call for add an event 
+    //api call for add an event
     $scope.addEvent = function(){
         console.log($scope.event);
         $http({
@@ -150,7 +111,7 @@ myApp.controller('EventsController', ['$scope', '$http', '$location', '$routePar
         });
     }
 
-    //api call for update an event 
+    //api call for update an event
     $scope.updateEvent = function(){
         var id = $routeParams.id;
         var newEvent={
